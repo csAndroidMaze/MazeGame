8d66d6c8996f7963ad435b1e4b6c0e99e301f966
Alexus McGriff <mcgriffale@gmail.com>
2018-05-16 19:15:47 -0400

    Merge branch 'master' of https://github.com/csAndroidMaze/MazeGame
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/android/cs/spring18/mazegame/monsterLevelOne.java
+++ app/src/main/java/android/cs/spring18/mazegame/monsterLevelOne.java
@@ -1,12 +1,11 @@
 package android.cs.spring18.mazegame;
-import java.util.ArrayList;
 import java.util.Random;
 public class monsterLevelOne {
     private int xPos;
     private int yPos;
 
-    private int xMoveDist=25;
-    private int yMoveDist=25;
+    private int xMoveDist=50;
+    private int yMoveDist=50;
 
     private int directionFaced=0;
 
@@ -17,47 +16,6 @@ public class monsterLevelOne {
         yPos=startY;
     }
 
-    public ArrayList<Integer> getXYChangeOnMove(){
-        ArrayList cPos=new ArrayList<Integer>();
-        int xMov=0;
-        int yMov=0;
-        if(directionFaced==0){
-            yMov=yMoveDist;
-        }
-        //Facing up
-        if(directionFaced==1){
-            yMov=-yMoveDist;
-        }
-        //Facing right
-        if(directionFaced==2){
-            xMov=xMoveDist;
-        }
-        //Facing left
-        if(directionFaced==3){
-            xMov=-xMoveDist;
-        }
-        cPos.add(xMov);
-        cPos.add(yMov);
-        return cPos;
-    }
-    public void reverseMove(){
-        //Facing down
-        if(directionFaced==0){
-            yPos-=yMoveDist;
-        }
-        //Facing up
-        else if(directionFaced==1){
-            yPos+=yMoveDist;
-        }
-        //Facing right
-        else if(directionFaced==2){
-            xPos-=xMoveDist;
-        }
-        //Facing left
-        else if(directionFaced==3){
-            xPos+=xMoveDist;
-        }
-    }
     public void setXMoveSpeed(int xSpeed){
         xMoveDist=xSpeed;
     }
